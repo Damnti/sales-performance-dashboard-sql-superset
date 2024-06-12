aa7f4aa39c6713f9a1de5d34e3976f018c16f7c8
Damnti <148279095+Damnti@users.noreply.github.com>
2024-06-12 19:23:46 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,14 @@
 SELECT
-COUNT(*) AS customers_count -- считаем количество строчек
+COUNT(*) AS customers_count -- считаем количество строк
 FROM customers;
+
+SELECT concat(emp.first_name, ' ', emp.last_name) AS seller, -- объединяем имя и фамилию
+COUNT(sales_id) AS operations, -- считаем количество продаж
+FLOOR(SUM(s.quantity * p.price)) AS income -- обрезаем сумму выручки
+FROM sales s
+INNER JOIN products p ON -- присоединяем таблицу
+s.product_id = p.product_id
+INNER JOIN employees emp ON -- присоединяем таблицу
+s.sales_person_id = emp.employee_id
+GROUP BY s.sales_person_id, emp.first_name, emp.last_name -- выполняем группировку
+ORDER BY 3 DESC; -- сортируем по третьему столбцу в порядке убывания
