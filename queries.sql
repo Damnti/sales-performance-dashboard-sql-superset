4f595f30a13aca524d4e44ff6e405d8b71cc35f8
Damnti <148279095+Damnti@users.noreply.github.com>
2024-06-12 19:41:26 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -12,3 +12,17 @@ INNER JOIN employees emp ON -- присоединяем таблицу
 s.sales_person_id = emp.employee_id
 GROUP BY s.sales_person_id, emp.first_name, emp.last_name -- выполняем группировку
 ORDER BY 3 DESC; -- сортируем по третьему столбцу в порядке убывания
+
+SELECT CONCAT(emp.first_name, ' ', emp.last_name) AS seller, -- объединяем имя и фамилию
+FLOOR(AVG(s.quantity * p.price)) as average_income -- обрезаем среднее значение выручки
+FROM sales s
+INNER JOIN products p ON -- присоединяем таблицу
+s.product_id = p.product_id
+INNER JOIN employees emp ON -- присоединяем таблицу
+s.sales_person_id = emp.employee_id
+GROUP BY s.sales_person_id, emp.first_name, emp.last_name -- выполняем группировку
+HAVING AVG(s.quantity * p.price) < (select AVG(s.quantity * p.price) 
+									FROM sales s
+									INNER JOIN products p ON
+									s.product_id = p.product_id)
+ORDER BY 2; -- сортируем по третьему столбцу в порядке убывания
