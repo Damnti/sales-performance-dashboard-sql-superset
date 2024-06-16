6e72b142ce2c6a0668b2de5bafeba08865ae9064
Damnti <148279095+Damnti@users.noreply.github.com>
2024-06-16 14:20:04 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
+-- Проект Продажи (4)
+
 SELECT
 COUNT(*) AS customers_count -- считаем количество строк
 FROM customers;
+
+-- Проект Продажи (5)
+
+-- top_10_total_income
 
 SELECT concat(emp.first_name, ' ', emp.last_name) AS seller, -- объединяем имя и фамилию
 COUNT(sales_id) AS operations, -- считаем количество продаж
@@ -12,6 +18,8 @@ INNER JOIN employees emp ON -- присоединяем таблицу
 s.sales_person_id = emp.employee_id
 GROUP BY s.sales_person_id, emp.first_name, emp.last_name -- выполняем группировку
 ORDER BY 3 DESC; -- сортируем по третьему столбцу в порядке убывания
+
+-- lowest_average_income
 
 SELECT CONCAT(emp.first_name, ' ', emp.last_name) AS seller, -- объединяем имя и фамилию
 FLOOR(AVG(s.quantity * p.price)) as average_income -- обрезаем среднее значение выручки
@@ -26,3 +34,8 @@ HAVING AVG(s.quantity * p.price) < (select AVG(s.quantity * p.price)
 				    INNER JOIN products p ON
 				    s.product_id = p.product_id)
 ORDER BY 2; -- сортируем по третьему столбцу в порядке возрастания
+
+-- day_of_the_week_income
+
+
+
